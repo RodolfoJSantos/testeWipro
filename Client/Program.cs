756488592a87e18f42199048bb22962e954f90fc
rodolfo santos <rodjsant@outlook.com>
2021-03-04 13:41:51 -0300

    inicial

---
+++ Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader leitor = new StreamReader("DadosMoeda.csv");
+
+            string line;
+            while ((line = leitor.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
